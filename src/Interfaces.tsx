afb11b6ab47e4f281531e778f819c9dee849e864
samau3 <samau415@gmail.com>
2021-10-25 16:32:39 -0700

    added code review comment and changes

--- src/Interfaces.tsx
+++ src/Interfaces.tsx
@@ -1,19 +1,19 @@
-interface BoxInterface {
+// could call file .ts
+
+export interface BoxInterface {
     width: string;
     height: string;
     backgroundColor: string;
 }
 
-interface BoxesInterface extends BoxInterface {
+export interface BoxesInterface extends BoxInterface {
     id: string;
   }
 
-interface BoxProps extends BoxesInterface{
+export interface BoxProps extends BoxesInterface{
     remove: (id: string) => void;
   }
 
-interface NewBoxProps {
+export interface NewBoxProps {
     createBox: (box: BoxesInterface)=>void;
   }
-
-export type {BoxInterface, BoxesInterface, BoxProps, NewBoxProps};
